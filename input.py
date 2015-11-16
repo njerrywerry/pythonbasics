fe01c04a97761b1fac75de2d2c5c32fb70fe9412
Sylvia <sylviakieha@gmail.com>
2015-11-16 13:45:46 +0300

    getting user input

---
+++ input.py
@@ -0,0 +1,11 @@
+print "How old are you?",
+#converts string input to integer
+age = int(raw_input())
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "You are %d years old, %s inches tall and %s kgs heavy." %(age, height, weight)
+age = age + 2
+print "You will be %d years old in two years." %age
