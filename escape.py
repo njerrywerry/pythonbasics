262a164d107b1726274b74eb96cf77ded00bfeee
Sylvia <sylviakieha@gmail.com>
2015-11-16 13:21:01 +0300

    escape sequences

---
+++ escape.py
@@ -0,0 +1,16 @@
+#escape sequences
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* fishies
+\t* Catnip\n\t* grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
