cd8b57aeb4d0687b5ce7a521757e568304dcede3
Sylvia <sylviakieha@gmail.com>
2015-11-17 09:09:57 +0300

    another way to get user input

--- input.py
+++ input.py
@@ -9,3 +9,10 @@ weight = raw_input()
 print "You are %d years old, %s inches tall and %s kgs heavy." %(age, height, weight)
 age = age + 2
 print "You will be %d years old in two years." %age
+
+#another way to get user input
+age = int(raw_input("How old are you? "))
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "You are %d years old, %s inches tall and %s kgs heavy" %(age, height, weight)
