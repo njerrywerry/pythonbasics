a5f830e94ce919eb05e200c42872b88f84332a30
Sylvia <sylviakieha@gmail.com>
2015-11-17 10:46:09 +0300

    opening files

---
+++ file.py
@@ -0,0 +1,18 @@
+#reading from files
+from sys import argv
+
+script, filename = argv
+
+#opening the file
+txt = open(filename)
+print "Here's your file %r:" %filename
+#reading from the file
+print txt.read()
+
+print "Type the filename again:"
+file_again = raw_input(">")
+
+#opening the file
+txt_again = open(file_again)
+#reading from the file
+print txt_again.read()
