3322884013789354da2cfaaf7ebfb20b3920cfec
Sylvia <sylviakieha@gmail.com>
2015-11-18 15:52:48 +0300

    more on functions

---
+++ function2.py
@@ -0,0 +1,18 @@
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
+    print "You have %d cheeses!" %cheese_count
+    print "You have %d boxes of crackers!" %boxes_of_crackers
+    print "That's enough for a party!"
+
+print "We can just give the function numbres directly:"
+cheese_and_crackers(20, 30)
+
+print "OR we can use variables from out script:"
+amount_of_cheese = 10
+amount_of_crackers = 50
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+print "We can even do math"
+cheese_and_crackers(10 + 20, 5 + 6)
+
+print "we can combine variables and math"
+cheese_and_crackers(amount_of_cheese + 3, amount_of_crackers + 5)
