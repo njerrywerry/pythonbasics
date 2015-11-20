7a940baaeecea726cd1b2e734b99d17d2c89e66a
Sylvia <sylviakieha@gmail.com>
2015-11-20 12:07:09 +0300

    functions with return statements

---
+++ function4.py
@@ -0,0 +1,30 @@
+#functions with return statements
+def add(a, b):
+    print "ADDING %d + %d" %(a, b)
+    return a + b
+
+def subtract(a, b):
+    print "SUBTRACTING %d - %d" %(a, b)
+    return a - b
+
+def multiply(a, b):
+    print "MULTIPLYING %d * %d" %(a, b)
+    return a * b
+
+def divide(a, b):
+    print "DIVIDING %d / %d" %(a, b)
+    return a / b
+
+print "Let's do some math with just functions"
+
+age = add(16, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(260, 2)
+
+print "Age: %d, Height: %d, Weight: %d, IQ: %d" %(age, height, weight, iq)
+
+#god status
+print "A puzzle:"
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+print "That becomes: ", what, "Can you do it by hand?"
