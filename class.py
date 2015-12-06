fc1b8e461e64adcfb976f16bd9df1df8a4a0eaf8
Sylvia <sylviakieha@gmail.com>
2015-12-06 19:01:37 +0300

    added english phrases to code for understanding

--- class.py
+++ class.py
@@ -1,19 +1,19 @@
 class Song(object):
-
+    # instance variable
     def __init__(self, lyrics):
         self.lyrics = lyrics
-
+    # object method
     def sing_me_a_song(self):
         for line in self.lyrics:
             print line
-
+# creating song objects
 happy_bday = Song(["Happy birthday to you",
                    "I don't want to get sued",
                    "So I'll stop right there"])
 
 bulls_on_parade = Song(["They rally around tha family",
                         "With pockets full of shells"])
-
+# calling object methods
 happy_bday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
